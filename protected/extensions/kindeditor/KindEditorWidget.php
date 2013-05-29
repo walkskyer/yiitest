c16f93a6d93a16e112ec041fcb7aad57509b1d72
weijie <walkskyer@88jianshen.com>
2013-05-29 17:14:01 +0800

    修正kindEditor扩展的items属性默认值。

--- protected/extensions/kindeditor/KindEditorWidget.php
+++ protected/extensions/kindeditor/KindEditorWidget.php
@@ -65,10 +65,14 @@ class KindEditorWidget extends CInputWidget
         'allowImageUpload'=>true,
         'allowFileManager'=>true,
         'items'=>array(
-            'fontname', 'fontsize', '|', 'forecolor', 'hilitecolor', 'bold', 'italic',
-            'underline', 'removeformat', '|', 'justifyleft', 'justifycenter',
-            'justifyright', 'insertorderedlist','insertunorderedlist', '|',
-            'emoticons', 'image', 'link',),
+            'source', '|', 'undo', 'redo', '|', 'preview', 'print', 'template', 'code', 'cut', 'copy', 'paste',
+            'plainpaste', 'wordpaste', '|', 'justifyleft', 'justifycenter', 'justifyright',
+            'justifyfull', 'insertorderedlist', 'insertunorderedlist', 'indent', 'outdent', 'subscript',
+            'superscript', 'clearhtml', 'quickformat', 'selectall', '|', 'fullscreen', '/',
+            'formatblock', 'fontname', 'fontsize', '|', 'forecolor', 'hilitecolor', 'bold',
+            'italic', 'underline', 'strikethrough', 'lineheight', 'removeformat', '|', 'image', 'multiimage',
+            'flash', 'media', 'insertfile', 'table', 'hr', 'emoticons', 'baidumap', 'pagebreak',
+            'anchor', 'link', 'unlink', '|', 'about',),
     );
 
 	/**
